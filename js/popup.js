ceb6bcac9dc1734b053b371c23ecff865fa8f7e0
Ramzan <mramzanstv@gmail.com>
2024-03-14 07:08:16 +0500

    change all links m-ramzan786 to ramzancodeclub

--- js/popup.js
+++ js/popup.js
@@ -7,7 +7,7 @@ const data = [
     description: "I created a simple blog website as part of my frontend development practice, utilizing a combination of HTML, CSS, and JavaScript. The primary goal was to enhance my skills in CSS Grid and building responsive and visually appealing web interfaces.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://blog-rouge-zeta.vercel.app/",
-    github: "https://github.com/m-ramzan786/Blog",
+    github: "https://github.com/ramzancodeclub/Blog",
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ const data = [
     description: "I made a blog website for people who are deafblind using HTML, CSS, and JavaScript. My main goal was to get better at making the front part of websites and to make a site that's easy to use and interesting. I also put YouTube videos to make it more interesting for different kinds of users.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://deafblind.vercel.app/",
-    github: "https://github.com/m-ramzan786/deafblind",
+    github: "https://github.com/ramzancodeclub/deafblind",
   },
   {
     id: 3,
@@ -27,7 +27,7 @@ const data = [
     description: "I crafted a website for a digital agency to enhance my frontend development skills. Using HTML, CSS, and JavaScript, the focus was on creating a professional and functional platform to showcase the agency's services. The project aimed to develop my ability to design and implement effective digital interfaces.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://creative-digital-agency-six.vercel.app/",
-    github: "https://github.com/m-ramzan786/creative-digital-agency",
+    github: "https://github.com/ramzancodeclub/creative-digital-agency",
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ const data = [
     description: "Crafted a captivating multipage space travel website inspired by Frontend Mentor and guided by Kevin Powell's Scrimba tutorials. Utilized HTML, CSS (employing grid and flexbox), and JavaScript to create a dynamic tab system for seamless exploration of diverse space-related topics.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://space-tourism-tau-indol.vercel.app/",
-    github: "https://github.com/m-ramzan786/Space-Tourism",
+    github: "https://github.com/ramzancodeclub/Space-Tourism",
   },
   {
     id: 5,
@@ -47,7 +47,7 @@ const data = [
     description: "Crafted a responsive single-page website dedicated to a pizza restaurant using HTML, CSS, Bootstrap, and JavaScript. With a perfect blend of design and functionality, the site showcases the restaurant's offerings, ensuring an engaging and visually pleasing experience for visitors.",
     technologies: ["HTML", "CSS", "Bootstrap", "JavaScript"],
     link: "https://foodler.vercel.app/",
-    github: "https://github.com/m-ramzan786/Foodler",
+    github: "https://github.com/ramzancodeclub/Foodler",
   },
   {
     id: 6,
@@ -57,7 +57,7 @@ const data = [
     description: "Designed a responsive single-page IT solution website using HTML, CSS, Bootstrap, and JavaScript. Incorporated the animate.style library for on-scroll animations, enhancing user engagement. The result is a visually appealing platform that effectively communicates IT services with a modern and dynamic touch.",
     technologies: ["HTML", "CSS", "Bootstrap", "JavaScript"],
     link: "https://it-solution-mu.vercel.app/",
-    github: "https://github.com/m-ramzan786/IT-Solution.git",
+    github: "https://github.com/ramzancodeclub/IT-Solution.git",
   },
   {
     id: 7,
@@ -67,17 +67,17 @@ const data = [
     description: "Crafted a serene single-page yoga and meditation website for frontend practice using HTML, CSS, and Bootstrap. The design prioritizes responsiveness and simplicity, providing a calming online space for users to explore and engage in mindful practices.",
     technologies: ["HTML", "CSS", "Bootstrap"],
     link: "https://yogawise.vercel.app/",
-    github: "https://github.com/m-ramzan786/Yogawise.git",
+    github: "https://github.com/ramzancodeclub/Yogawise.git",
   },
   {
     id: 8,
     title: "E-Commerce Shop",
     subtitle: "Website",
     image: "img/E-commerce.JPG",
-    description: "Developed a responsive single-page e-commerce website for frontend practice, incorporating HTML, CSS, and Tailwind CSS framework. The design seamlessly blends aesthetics with functionality, showcasing the versatility of Tailwind CSS and honing frontend skills for an engaging online shopping experience.",
-    technologies: ["HTML", "CSS", "tailwindcss"],
+    description: "Developed a responsive single-page e-commerce website for frontend practice, incorporating HTML and Tailwind CSS framework. The design seamlessly blends aesthetics with functionality, showcasing the versatility of Tailwind CSS and honing frontend skills for an engaging online shopping experience.",
+    technologies: ["HTML", "tailwindcss"],
     link: "https://e-commerce-green-omega.vercel.app/",
-    github: "https://github.com/m-ramzan786/E-Commerce.git",
+    github: "https://github.com/ramzancodeclub/E-Commerce.git",
   },
   {
     id: 9,
@@ -87,7 +87,7 @@ const data = [
     description: "Created a dynamic counter app for JavaScript practice. The application utilizes JavaScript to increment, decrement, and display numerical values. Through this project, strengthened programming skills and gained hands-on experience in building interactive and functional features with JavaScript.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://counter-app-wine-eta.vercel.app/",
-    github: "https://github.com/m-ramzan786/Counter-App.git",
+    github: "https://github.com/ramzancodeclub/Counter-App.git",
   },
   {
     id: 10,
@@ -97,7 +97,7 @@ const data = [
     description: "Developed a functional calculator app for JavaScript practice. Leveraging JavaScript, the app performs basic arithmetic operations with a user-friendly interface. This project enhanced coding proficiency, providing hands-on experience in creating interactive and practical applications using JavaScript.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://calculator-sigma-one-31.vercel.app/",
-    github: "https://github.com/m-ramzan786/Calculator.git",
+    github: "https://github.com/ramzancodeclub/Calculator.git",
   },
   {
     id: 11,
@@ -107,7 +107,7 @@ const data = [
     description: "Designed a tip calculator app for JavaScript practice. Using JavaScript, the app computes tip amounts based on user inputs, offering a practical tool for calculating tips in a user-friendly manner. This project enhanced coding skills and application development using JavaScript.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://tip-calculator-liard-one.vercel.app/",
-    github: "https://github.com/m-ramzan786/Tip-Calculator.git",
+    github: "https://github.com/ramzancodeclub/Tip-Calculator.git",
   },
   {
     id: 12,
@@ -117,7 +117,7 @@ const data = [
     description: "Crafted a countdown timer app in JavaScript for an upcoming event. The app dynamically counts down the days, hours, minutes, and seconds, building anticipation for the event. This project honed JavaScript skills, offering a practical and visually engaging way to track time.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://count-down-timer-chi.vercel.app/",
-    github: "https://github.com/m-ramzan786/Count-Down-Timer.git",
+    github: "https://github.com/ramzancodeclub/Count-Down-Timer.git",
   },
   {
     id: 13,
@@ -127,7 +127,7 @@ const data = [
     description: "Developed a daily goal app in JavaScript for adding and tracking daily tasks. The app provides a simple interface to input tasks, enhancing organization and productivity. This project strengthened JavaScript skills, emphasizing practical application for daily task management.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://daily-goal.vercel.app/",
-    github: "https://github.com/m-ramzan786/Daily-Goal.git",
+    github: "https://github.com/ramzancodeclub/Daily-Goal.git",
   },
   {
     id: 14,
@@ -137,7 +137,7 @@ const data = [
     description: "Built a words generator app in JavaScript that generates random words based on user input. This practical app enhances language skills and coding proficiency, allowing users to customize the number of generated words for diverse applications and learning scenarios.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://words-generator-ochre.vercel.app/",
-    github: "https://github.com/m-ramzan786/Words-Generator.git",
+    github: "https://github.com/ramzancodeclub/Words-Generator.git",
   },
   {
     id: 15,
@@ -147,7 +147,7 @@ const data = [
     description: "Developed a Blackjack game in JavaScript with guidance from Scrimba tutorials. This interactive game incorporates essential Blackjack rules, providing an immersive and educational experience. The project honed JavaScript skills through hands-on application, reinforcing game development concepts.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://blackjack-game-liard.vercel.app/",
-    github: "https://github.com/m-ramzan786/Blackjack-Game.git",
+    github: "https://github.com/ramzancodeclub/Blackjack-Game.git",
   },
   {
     id: 16,
@@ -157,7 +157,7 @@ const data = [
     description: "Crafted a dice roll game in JavaScript. This interactive game emulates the thrill of rolling dice, offering a user-friendly experience. Through this project, honed skills in JavaScript, focusing on randomization and creating an engaging, playable application",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://dice-game-teal-gamma.vercel.app/",
-    github: "https://github.com/m-ramzan786/Dice-Game.git",
+    github: "https://github.com/ramzancodeclub/Dice-Game.git",
   },
   {
     id: 17,
@@ -167,7 +167,7 @@ const data = [
     description: "Developed a classic Snake game in JavaScript. This project involved creating an interactive and visually engaging gaming experience. Through coding and implementing game logic, enhanced JavaScript skills were applied, resulting in a fun and challenging game.",
     technologies: ["HTML", "CSS", "JavaScript"],
     link: "https://snake-game-phi-two.vercel.app/",
-    github: "https://github.com/m-ramzan786/Snake-Game.git",
+    github: "https://github.com/ramzancodeclub/Snake-Game.git",
   },
   {
     id: 18,
@@ -176,8 +176,8 @@ const data = [
     image: "img/Leads Tracker Chrome Extension.jpg",
     description: "Built a Leads Tracker Chrome extension in JavaScript for saving tab URLs and user input. This practical extension streamlines lead management by allowing users to effortlessly capture and organize information. The project strengthened JavaScript skills with a focus on browser extension development.",
     technologies: ["HTML", "CSS", "JavaScript"],
-    link: "https://github.com/m-ramzan786/Leads_Tracker-Chrome-Extension.git",
-    github: "https://github.com/m-ramzan786/Leads_Tracker-Chrome-Extension.git",
+    link: "https://github.com/ramzancodeclub/Leads_Tracker-Chrome-Extension.git",
+    github: "https://github.com/ramzancodeclub/Leads_Tracker-Chrome-Extension.git",
   },
 ];
 
